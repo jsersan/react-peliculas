ef6667f0842fe88b030a5385cc2faebea4e2a5ff
jsersan <jsersan@gmail.com>
2022-08-01 19:35:28 +0200

    Update httpClient.js

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -3,7 +3,7 @@ const API = "https://api.themoviedb.org/3";
 export function get(path){
     return fetch(API + path, {
         headers: {
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTc2YzJiMjJkZmJmMDUxNGFjYzdhYzE3NWE4NjVhOCIsInN1YiI6IjYxZWFlZWQxODQ4ZWI5MDBmMDM3MzU5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MdGQnCJweURTyCl6yPGEwadTO87sGACe9LIfNqSnTXk',
+          Authorization: 'Bearer API-Key',
           "Content-Type": 'application/json;charset=utf-8',    
           },
       })
